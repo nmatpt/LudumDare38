d18d41b41eac11394bf2e4ac436b95f762d1a31d
Vasco Correia <maxteel1@gmail.com>
2017-04-22 16:17:21 +0100

    Selectable tiles via Map Manager. Resettable map.

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,41 +1,85 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapManager : MonoBehaviour {
 
+    public Camera gameCamera;
+
     public int mapRadius;
 
-    public GameObject hex;
+    public GameObject hexTemplate;
     public float hexRadius;
+
+    private TileMatrix matrix;
+    private GameObject selectedTile;
+    
 
 	// Use this for initialization
 	void Start () {
-        
-        for (int i=-mapRadius; i <= mapRadius; i++)
-        {
-            for (int j=-mapRadius; j <= mapRadius; j++)
-            {
-                for (int k=-mapRadius; k <= mapRadius; k++)
-                {
-                    if (i+j+k != 0)
-                    {
-                        continue;
-                    }
-                    Vector3 cubeVec = new Vector3(i, j, k);
-                    Vector2 axialVec = CubeToAxialCoordinates(cubeVec);
-                    Vector2 screenVec = AxialToPointyTopSceneCoordinates(axialVec);
-                    //print(cubeVec + " " + axialVec + " " + screenVec);
-                    Instantiate(hex, new Vector3(screenVec.x, screenVec.y, 0), Quaternion.identity);
-                }
-            }
-        }
+
+        matrix = generateMap();
+
+        selectedTile = null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+            //print(Input.mousePosition + " " + mousePosition + " " + gameCamera.ScreenToWorldPoint(Input.mousePosition) + " " + FlatTopSceneToAxialCoordinates(mousePosition));
+            Vector2 selectedTileCoordinates = FlatTopSceneToAxialCoordinates(mousePosition);
+            GameObject tile = matrix.GetValue( (int)selectedTileCoordinates.x, (int)selectedTileCoordinates.y);
+            if (tile != null && tile != selectedTile)
+            {
+                if (selectedTile != null)
+                {
+                    selectedTile.GetComponent<TileState>().UnselectTile();
+                }
+                tile.GetComponent<TileState>().SelectTile();
+                selectedTile = tile;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            selectedTile = null;
+            foreach (GameObject go in matrix)
+            {
+                Destroy(go);
+            }
+            matrix = generateMap();
+        }
 	}
+
+    private TileMatrix generateMap()
+    {
+        TileMatrix tileMatrix = new TileMatrix();
+
+        for (int i = -mapRadius; i <= mapRadius; i++)
+        {
+            for (int j = -mapRadius; j <= mapRadius; j++)
+            {
+                for (int k = -mapRadius; k <= mapRadius; k++)
+                {
+                    if (i + j + k != 0)
+                    {
+                        continue;
+                    }
+                    Vector3 cubeVec = new Vector3(i, j, k);
+                    Vector2 axialVec = CubeToAxialCoordinates(cubeVec);
+                    //Vector2 screenVec = AxialToPointyTopSceneCoordinates(axialVec);
+                    Vector2 screenVec = AxialToFlatTopSceneCoordinates(axialVec);
+                    GameObject hex = Instantiate(hexTemplate, new Vector3(screenVec.x, screenVec.y, 0), Quaternion.identity);
+                    tileMatrix.SetValue((int)axialVec.x, (int)axialVec.y, hex);
+                }
+            }
+        }
+        return tileMatrix;
+    }
 
     private Vector2 CubeToAxialCoordinates(Vector3 cubeCoordinates)
     {
@@ -65,5 +109,105 @@ public class MapManager : MonoBehaviour {
         float y = hexRadius * 3 / 2 * axialCoordinates.y;
 
         return new Vector2(x, y);
+    }
+
+    private Vector2 FlatTopSceneToAxialCoordinates(Vector2 sceneCoordinates)
+    {
+        float x = sceneCoordinates.x * 2 / 3 / hexRadius;
+        float y = (-sceneCoordinates.x / 3 + Mathf.Sqrt(3) / 3 * sceneCoordinates.y) / hexRadius;
+        return roundAxialCoordinates(new Vector2(x, y));
+    }
+
+    private Vector2 PointyTopSceneToAxialCoordinates(Vector2 sceneCoordinates)
+    {
+        float x = (sceneCoordinates.x * Mathf.Sqrt(3) / 3 - sceneCoordinates.y / 3) / hexRadius;
+        float y = sceneCoordinates.y * 2 / 3 / hexRadius;
+        return roundAxialCoordinates(new Vector2(x, y));
+    }
+
+    private Vector3 roundCubeCoordinates(Vector3 coordinates)
+    {
+        float x = Mathf.Round(coordinates.x);
+        float y = Mathf.Round(coordinates.y);
+        float z = Mathf.Round(coordinates.z);
+
+        float x_diff = Mathf.Abs(x - coordinates.x);
+        float y_diff = Mathf.Abs(y - coordinates.y);
+        float z_diff = Mathf.Abs(z - coordinates.z);
+
+        if (x_diff > y_diff && x_diff > z_diff)
+        {
+            x = -y - z;
+        }
+        else if (y_diff > z_diff)
+        {
+            y = -x - z;
+        }
+        else
+        {
+            z = -x - y;
+        }
+
+        return new Vector3(x, y, z);
+    }
+
+    private Vector2 roundAxialCoordinates(Vector2 coordinates)
+    {
+        return CubeToAxialCoordinates(roundCubeCoordinates(AxialToCubeCoordinates(coordinates)));
+    }
+
+
+    private class TileMatrix : IEnumerable<GameObject>
+    {
+        private Dictionary<int, Dictionary<int, GameObject>> matrix;
+
+        public TileMatrix()
+        {
+            matrix = new Dictionary<int, Dictionary<int, GameObject>>();
+        }
+
+        public void SetValue(int x, int y, GameObject value)
+        {
+            Dictionary<int, GameObject> dict = null;
+            try
+            {
+                dict = matrix[x];
+            }
+            catch (System.Exception)
+            {
+                dict = new Dictionary<int, GameObject>();
+                matrix[x] = dict;
+            }
+
+            dict[y] = value;
+        }
+
+        public GameObject GetValue(int x, int y)
+        {
+            try
+            {
+                return matrix[x][y];
+            }
+            catch (System.Exception)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerator<GameObject> GetEnumerator()
+        {
+            foreach (int x in matrix.Keys)
+            {
+                foreach (int y in matrix[x].Keys)
+                {
+                    yield return matrix[x][y];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
---
+++ Assets/Scripts/TileState.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileState : MonoBehaviour {
+
+	public Sprite defaultSprite;
+	public Sprite selectedSprite;
+	private SpriteRenderer spriteRenderer;
+	private bool isSelected = false;
+
+	void Awake ()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		spriteRenderer.sprite = defaultSprite;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	//void OnMouseDown(){
+	//	if (!isSelected) {
+	//		spriteRenderer.sprite = selectedSprite;
+	//		isSelected = true;
+	//	} else {
+	//		spriteRenderer.sprite = defaultSprite;
+	//		isSelected = false;
+	//	}
+	//}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void SelectTile()
+    {
+        spriteRenderer.sprite = selectedSprite;
+        isSelected = true;
+    }
+
+    public void UnselectTile()
+    {
+        spriteRenderer.sprite = defaultSprite;
+        isSelected = false;
+    }
+
+}
+
